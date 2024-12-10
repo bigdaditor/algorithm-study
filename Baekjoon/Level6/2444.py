2d1ba39f8dd38b2a796bea8f31b6ba3b67d1fab6
bigdaditor <bigdaditor1015@gmail.com>
2024-12-10 11:38:59 +0900

    백준 5단계 풀이 완료된 문제

--- Baekjoon/Level6/2444.py
+++ Baekjoon/Level6/2444.py
@@ -3,12 +3,28 @@ input = sys.stdin.readline
 
 n = int(input())
 line = 2*n-1
-for _ in range(line):
+
+for i in range(line):
+    # 별 찍기 라인
+    idx = i + 1
     blank = ''
     star = ''
-    for i in range(n):
-        for j in range(n-1-i):
-            blank = blank + ' '
-        for k in range(i):
+
+    # 입력된 수보다 작을 때는 인덱스
+    if idx < n:
+        blank_idx = n - idx
+        star_idx = idx
+    else:
+        blank_idx = idx - n
+        star_idx = n-blank_idx
+
+    for j in range(blank_idx):
+        blank = blank + ' '
+    for k in range(star_idx):
+        # 첫 줄과 마지막 줄은 하나만 추가
+        if k == 0 or k == line:
             star = star + '*'
-        print(star)
+        else :
+            star = star + '**'
+
+    print(blank + star)
