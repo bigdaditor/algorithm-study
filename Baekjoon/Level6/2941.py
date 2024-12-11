fd640d7b3a681753abed0ffb1ad9794e0c039a7e
bigdaditor <bigdaditor1015@gmail.com>
2024-12-11 16:19:07 +0900

    백준 6단계 풀이 중인 문제

--- Baekjoon/Level6/2941.py
+++ Baekjoon/Level6/2941.py
@@ -2,5 +2,17 @@ import sys
 
 input = sys.stdin.readline
 string = input().rstrip()
-croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+
+def count_croatia_alpha(string):
+    croatia = ['c=', 'c-', 'dz=', 'd-', 'lj', 'nj', 's=', 'z=']
+    cnt = 0
+    for c in croatia:
+        if c in string:
+            cnt += 1
+            string = string.replace(c, '')
+        else :
+
+            count_croatia_alpha(string)
+    if string == '':
+        return cnt
 
