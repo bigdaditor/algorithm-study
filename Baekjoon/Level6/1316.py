9deee31f0e8f6ad8e096c96aa3b929942bc0d7db
bigdaditor <bigdaditor1015@gmail.com>
2024-12-13 09:25:37 +0900

    백준 6단계 풀이 중인 문제

---
+++ Baekjoon/Level6/1316.py
@@ -0,0 +1,33 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+list = []
+cnt = 0
+
+for _ in range(t):
+    string = input().rstrip()
+    flag = False
+
+    for i in range(len(string)):
+        if i < len(string)-1:
+            if string[i] == string[i+1]:
+                flag = True
+                list.append(string[i])
+            else :
+                if string[i] in list:
+                    flag = False
+                else:
+                    flag = True
+                    list.append(string[i])
+        else:
+            if string[i] in list:
+                flag = False
+            else:
+                flag = True
+                list.append(string[i])
+
+    if flag:
+        cnt += 1
+
+print(cnt)
