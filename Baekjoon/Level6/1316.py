c524fffb86b344b1f5b79dc0a88dd4a0fe287cfa
bigdaditor <bigdaditor1015@gmail.com>
2024-12-17 13:35:01 +0900

    백준 6단계 풀이 완료 문제

--- Baekjoon/Level6/1316.py
+++ Baekjoon/Level6/1316.py
@@ -2,11 +2,12 @@ import sys
 input = sys.stdin.readline
 
 t = int(input().rstrip())
-list = []
+
 cnt = 0
 
 for _ in range(t):
     string = input().rstrip()
+    dictionary = {}
     flag = False
 
     if len(string) == 1:
@@ -14,22 +15,21 @@ for _ in range(t):
         continue
 
     for i in range(len(string)):
-        if i < len(string)-1:
-            if string[i] == string[i+1]:
-                flag = True
-                list.append(string[i])
+        if string[i] in string:
+            if dictionary.get(string[i]) is None:
+                dictionary[string[i]] = 1
             else :
-                if string[i] in list:
-                    flag = False
-                else:
-                    flag = True
-                    list.append(string[i])
-        else:
-            if string[i] in list:
-                flag = False
-            else:
-                flag = True
-                list.append(string[i])
+                if string[i] == string[i-1]:
+                    dictionary[string[i]] = dictionary[string[i]]
+                else :
+                    dictionary[string[i]] = dictionary[string[i]] + 1
+
+    for i in dictionary.values():
+        if i > 1:
+            flag = False
+            break
+        else :
+            flag = True
 
     if flag:
         cnt += 1
