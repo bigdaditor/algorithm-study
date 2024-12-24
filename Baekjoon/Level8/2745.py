8e5719131e72fd25d5e432a113e21433b69ee655
bigdaditor <bigdaditor1015@gmail.com>
2024-12-24 16:02:54 +0900

    백준 8단계 풀이 완료 문제

---
+++ Baekjoon/Level8/2745.py
@@ -0,0 +1,52 @@
+import sys
+
+input = sys.stdin.readline
+
+alpha_dict = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    'A': 10,
+    'B': 11,
+    'C': 12,
+    'D': 13,
+    'E': 14,
+    'F': 15,
+    'G': 16,
+    'H': 17,
+    'I': 18,
+    'J': 19,
+    'K': 20,
+    'L': 21,
+    'M': 22,
+    'N': 23,
+    'O': 24,
+    'P': 25,
+    'Q': 26,
+    'R': 27,
+    'S': 28,
+    'T': 29,
+    'U': 30,
+    'V': 31,
+    'W': 32,
+    'X': 33,
+    'Y': 34,
+    'Z': 35
+}
+
+n,b = input().rstrip().split()
+b = int(b)
+
+sum = 0
+for i in range(len(n)-1,-1,-1):
+    sup = len(n)-i-1
+    sum = sum + (alpha_dict.get(n[i]) * b**sup)
+
+print(sum)
