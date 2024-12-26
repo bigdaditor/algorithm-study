b5d0f02a2293cdb135ba41dc31b134d09b59c38b
bigdaditor <bigdaditor1015@gmail.com>
2024-12-26 14:13:04 +0900

    백준 8단계 풀이 중인 문제

---
+++ Baekjoon/Level8/2292.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+
+if n == 1:
+    print(1)
+
+a = 1
+l = 2
+while True:
+    # l == 2
+    mx = 6 * a + 1
+    if n <= mx:
+        print(l)
+        break
+    a += l
+    l += 1
+
