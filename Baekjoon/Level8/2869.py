9cab8e96312e4f2f2ecf62940d2ce0e8a0eeeced
bigdaditor <bigdaditor1015@gmail.com>
2025-01-15 16:07:15 +0900

    백준 8단계 풀이 완료 문제 - 스스로 풀기 실패

--- Baekjoon/Level8/2869.py
+++ Baekjoon/Level8/2869.py
@@ -1,4 +1,3 @@
-import math
 import sys
 
 input = sys.stdin.readline
@@ -7,4 +6,10 @@ a,b,v = map(int,input().split())
 
 diff = a - b
 
-print(v/diff)
+if v < a:
+    print(1)
+else :
+    if (v-a)%diff == 0:
+        print(((v - a) // diff) + 1)
+    else :
+        print(((v - a) // diff) + 2)
